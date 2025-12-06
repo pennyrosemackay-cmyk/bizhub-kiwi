d9b624f029c385413891d9b19d224b1fbc5f0dc8
pennyrosemackay-cmyk <pennyrose.mackay@gmail.com>
2025-12-06 17:47:50 +1300

    Create layout.js

---
+++ app/layout.js
@@ -0,0 +1,10 @@
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <head>
+        <title>BizHub Kiwi</title>
+      </head>
+      <body>{children}</body>
+    </html>
+  )
+}
